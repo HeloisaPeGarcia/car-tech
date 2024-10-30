9efafd2e1ed6436223f2cdfeb48e53c2352a9f27
Heloisa Pereira Garcia <141871607+HeloisaPeGarcia@users.noreply.github.com>
2024-10-30 18:11:34 -0300

    editado2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-// src/App.js
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import Servicos from './components/Servicos';
 import Line from './components/Line';
 import Footer from './components/Footer';
+import Portfolio from './components/Portfolio';
+import Servicos from './components/Servicos';
+import Form from './components/Form';
+import { Routes, Route, Link } from 'react-router-dom'; // Adicione o Link
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <nav>
+        {/* Links para navegação */}
+        <Link to="/Portfolio">Portfolio</Link>
+        <Link to="/Form">Form</Link>
+      </nav>
       <section className="hero">
-        {/* Conteúdo da seção hero */}
+        <Routes>
+          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="/Form" element={<Form />} />
+          {/* Rota padrão (opcional) */}
+        
+        </Routes>
       </section>
-      <Servicos />
+      <Servicos/>
+      <Portfolio/>
       <Line />
       <Footer />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,27 @@
-// src/components/Header.js
+
 import React from 'react';
-import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube, FaLinkedin } from 'react-icons/fa';
 import '../App.css';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
-      <div className="logo">
-        <img src={require('../assets/logo.png')} alt="Logo da Empresa" className="logo-img" />
-      </div>
-      <nav>
-        <ul>
-          <li><a href="#services">Serviços</a></li>
-          <li><a href="#contact">Contato</a></li>
-          <li><a href="#portfolio">Portfólio</a></li>
-          <li><a href="#about">Sobre nós</a></li>
-        </ul>
-      </nav>
-      <div className="auth-buttons">
-        <button className="login">Login</button>
-        <button className="register">Register</button>
-      </div>
-    </header>
+    <div className="logo">
+      <img src={require('../assets/logo.png')} alt="Logo da Empresa" className="logo-img" />
+    </div>
+    <nav>
+      <ul>
+        <li><a href="#services">Serviços</a></li>
+        <li><Link to="/Form">Contato</Link></li>
+        <li><Link to="/Portfolio">Portfolio</Link></li>
+        <li><a href="#about">Sobre nós</a></li>
+      </ul>
+    </nav>
+    <div className="auth-buttons">
+      <button className="login">Login</button>
+      <button className="register">Register</button>
+    </div>
+  </header>
   );
 }
 
