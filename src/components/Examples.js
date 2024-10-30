7e687fdb5edb0fdd863c97f8a3dd36f465e83ece
RyDev <88353449+RyanGaspri@users.noreply.github.com>
2024-10-30 10:44:58 -0300

    editado2

---
+++ src/components/Examples.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Examples = () => (
+  <section>
+    <h2>Exemplos</h2>
+    <div className="examples">
+      <div>
+        <h4>Pintura metálica</h4>
+        <p>Qualidade de pintura de alto padrão...</p>
+      </div>
+      <div>
+        <h4>Body kit</h4>
+        <p>Customização completa do kit aerodinâmico...</p>
+      </div>
+      <div>
+        <h4>Fitas de LED</h4>
+        <p>Iluminação personalizada com fitas de LED..</p>
+      </div>
+      <div>
+        <h4>Adesivo esportivo</h4>
+        <p>Design personalizado com adesivo esportivo...</p>
+      </div>
+      <div>
+        <h4>Rebaixamento</h4>
+        <p>Suspensão ajustada para melhor performance...</p>
+      </div>
+    </div>
+  </section>
+);
+
+export default Examples;
